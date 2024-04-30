ca756adbc54a6489d3d0994e96a3e803aab3d01f
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-04-30 21:53:34 +0600

    Digits

---
+++ eolymp/Digits.cpp
@@ -0,0 +1,42 @@
+// /**
+//  *   author:  TAMIM IQBAL
+//  *   created: 30/04/2024 20:44:38
+// **/
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     long long n;
+//     cin>>n;
+//     long long count=0;
+//     if(n==0) count = 1;
+//     while(n>0)
+//     {
+//         n /= 10;
+//         count++;
+//     }
+//     cout<<count<<'\n';
+//     return 0;
+// }
+
+/**
+ *   author:  TAMIM IQBAL
+ *   created: 30/04/2024 20:56:54
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin>>s;
+    cout<<s.size()<<endl;
+
+    return 0;
+}
