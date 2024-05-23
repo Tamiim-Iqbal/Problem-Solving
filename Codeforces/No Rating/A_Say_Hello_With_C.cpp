4c863163dce3155e87ad0b9cf0bd46eb2dc546e3
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-05-24 03:25:40 +0600

    practice

---
+++ Codeforces/No Rating/A_Say_Hello_With_C.cpp
@@ -0,0 +1,16 @@
+/**
+ *   author:  TAMIM IQBAL
+ *   created: 01/05/2024 19:44:42
+**/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin>>s;
+    cout<<"Hello, "<<s<<endl;
+    return 0;
+}
